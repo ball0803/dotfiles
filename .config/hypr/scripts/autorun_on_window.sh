b013a94bdf7e8f40557b074bc0edf91a92cdf85d
ball0803 <hackball08@gmail.com>
2024-05-27 01:43:27 +0700

    change hypr config and eww

--- .config/hypr/scripts/autorun_on_window.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-function handle {
-  if [[ ${1:0:10} == "openwindow" ]] && [[ $1 == *"spotify"* ]]; then
-    spicetify watch -s
-  fi
-}
-
-socat - UNIX-CONNECT:/tmp/hypr/$(echo $HYPRLAND_INSTANCE_SIGNATURE)/.socket2.sock | while read line; do handle $line; done
