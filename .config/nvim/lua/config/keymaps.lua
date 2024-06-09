15cd2693fa89bb9e36789821410ec62dd7e5aed9
ball0803 <hackball08@gmail.com>
2024-06-09 17:20:23 +0700

    add neovim

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,24 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+-- increment/decrement numbers
+keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
+
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+
+-- halfpage vertical movement center
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
+
+keymap.set("n", "ss", "<cmd>split<Return>")
+keymap.set("n", "sv", "<cmd>vsplit<Return>")
+
+keymap.set("n", "<A-J>", "yyp")
+keymap.set("n", "<A-K>", "yyP")
+
+keymap.set("n", "U", "<cmd>UndotreeToggle<CR>")
---
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,26 @@
+-- add more treesitter parsers
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "bash",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "query",
+      "regex",
+      "rust",
+      "tsx",
+      "typescript",
+      "vim",
+      "yaml",
+      "tsx",
+      "c_sharp",
+      "typescript",
+    },
+  },
+}
