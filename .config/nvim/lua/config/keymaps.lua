f0a0f7da93af88b9fe447b51e0c119ed1ad55a01
ball0803 <hackball08@gmail.com>
2024-06-24 17:57:27 +0700

    update nvim dvorak

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -5,20 +5,49 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
+keymap.del({ "n", "x", "i", "s" }, "<C-s>")
+keymap.set('n', '<C-S>', ':w<CR>', { noremap = true, silent = true })
+keymap.set('i', '<C-S>', '<Esc>:w<CR>a', { noremap = true, silent = true })
+
+keymap.set("n", "h", "j")
+keymap.set("n", "t", "k")
+keymap.set("n", "n", "h")
+keymap.set("n", "s", "l")
+
+keymap.set("v", "h", "j")
+keymap.set("v", "t", "k")
+keymap.set("v", "n", "h")
+keymap.set("v", "s", "l")
+
+keymap.set('i', '<C-h>', '<Down>')
+keymap.set('i', '<C-t>', '<Up>')
+keymap.set('i', '<C-n>', '<Left>')
+keymap.set('i', '<C-s>', '<Right>')
+
+keymap.set("n", "<C-I>", "<C-I>")
+keymap.set("n", "<Tab>", "<Nop>")
+
+keymap.del('n', '<C-h>')
+keymap.del('n', '<C-j>')
+keymap.del('n', '<C-k>')
+keymap.del('n', '<C-l>')
+
+keymap.set('n', '<C-h>', '<C-w>j')
+keymap.set('n', '<C-t>', '<C-w>k')
+keymap.set('n', '<C-n>', '<C-w>h')
+keymap.set('n', '<C-s>', '<C-w>l')
+
 -- increment/decrement numbers
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
 
-keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+keymap.set("i", "ht", "<ESC>", { desc = "Exit insert mode with jk" })
 
 -- halfpage vertical movement center
 keymap.set("n", "<C-d>", "<C-d>zz")
 keymap.set("n", "<C-u>", "<C-u>zz")
 
-keymap.set("n", "ss", "<cmd>split<Return>")
-keymap.set("n", "sv", "<cmd>vsplit<Return>")
-
-keymap.set("n", "<A-J>", "yyp")
-keymap.set("n", "<A-K>", "yyP")
+keymap.set("n", "<A-H>", "yyp")
+keymap.set("n", "<A-T>", "yyP")
 
 keymap.set("n", "U", "<cmd>UndotreeToggle<CR>")
--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -7,9 +7,9 @@ return {
     local keymap = vim.keymap -- for conciseness
     local ui = require("harpoon.ui")
 
-    keymap.set("n", "hx", require("harpoon.mark").add_file)
-    keymap.set("n", "hn", ui.nav_next)
-    keymap.set("n", "hp", ui.nav_prev)
+    keymap.set("n", "<leader>hx", require("harpoon.mark").add_file)
+    keymap.set("n", "<leader>hn", ui.nav_next)
+    keymap.set("n", "<leader>hp", ui.nav_prev)
     keymap.set("n", "<leader>hm", "<cmd>Telescope harpoon marks<CR>")
     keymap.set("n", "<leader>hM", ui.toggle_quick_menu)
     keymap.set("n", "<a-1>", function() ui.nav_file(1) end)
